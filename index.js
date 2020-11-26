b200a72402579f8559312824da2d386681e2a229
hofry <hofry@yandex.ru>
2020-11-26 21:01:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ const github = require('@actions/github');
 const request = require('request');
 
 try {
+  if (core.getInput('tag') != '')
+    const tag = core.getInput('tag');
+  else
+    const tag = github.context.ref.replace('refs/tags/', '');
   const jenkinsUrl = core.getInput('jenkinsUrl');
   const env = core.getInput('env');
   const token = core.getInput('token');
   const jobName = core.getInput('job');
-  const params = JSON.parse( core.getInput('params'));
-
   
-  request.post({baseUrl: jenkinsUrl
-    , uri: 'job/' + env + '/' + jobName + '/buildWithParameters' 
-    , qs: params})
+  request.get({baseUrl: jenkinsUrl, uri: 'job/' + env + '/' + jobName + '/buildWithParameters?token=' + token + '&DOCKER_IMAGE_TAG=' + tag})
     
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 } catch (error) {
   core.setFailed(error.message);
 }
-
